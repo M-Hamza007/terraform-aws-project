2d78b90d7e05a1406260527877f2adb4eb3c8093
muhammadh <muhammad.hamza@xflowresearch.com>
2022-03-31 21:14:42 +0500

    update userdata file

--- modules_folder/data.sh
+++ modules_folder/data.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install httpd
+sudo apt-get upgrade
 sudo apt-get install nginx -y
 sudo echo "Hello, from terraform!!" >/var/www/html/index.nginx-debian.html
-sudo service httpd start
-chkconfig httpd on
